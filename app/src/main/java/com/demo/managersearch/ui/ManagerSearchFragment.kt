2e6b5b70eb13b834c6b75988c2dee3d4fb48f371
Anders Nielsen <anden1982@gmail.com>
2020-10-18 23:02:05 +0200

    fixed wrong import

--- app/src/main/java/com/demo/managersearch/ui/ManagerSearchFragment.kt
+++ app/src/main/java/com/demo/managersearch/ui/ManagerSearchFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import com.demo.managersearch.R
 import com.demo.managersearch.databinding.ManagerSearchFragmentBinding
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -41,7 +41,7 @@ class ManagerSearchFragment : Fragment() {
         binding.managerList.adapter = adapter
         binding.managerList.setHasFixedSize(true)
 
-        val dividerItemDecoration = DividerItemDecoration(context, LinearLayout.VERTICAL)
+        val dividerItemDecoration = DividerItemDecoration(context, VERTICAL)
         binding.managerList.addItemDecoration(dividerItemDecoration)
 
         return binding.root
