4e9a9a189cb6ff2f93712bef350bbf10b2d71cb3
Anders Nielsen <anden1982@gmail.com>
2020-10-18 22:35:43 +0200

    added basic UI test to test search input filtering

---
+++ app/src/androidTest/java/com/demo/managersearch/SearchTextInputTest.kt
@@ -0,0 +1,65 @@
+package com.demo.managersearch
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SearchTextInputTest {
+
+    lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setUp() {
+        scenario = launchActivity()
+    }
+
+    @Test
+    fun testSearchInputText() {
+
+        onView(withId(R.id.search_field)).perform(click())
+        onView(withId(R.id.search_field)).perform(typeText("ha"))
+        Thread.sleep(2000) // todo mock repository instead
+        onView(withId(R.id.manager_list)).check(RecyclerViewItemCountAssertion(2))
+
+        onView(withId(R.id.search_field)).perform(clearText())
+        Thread.sleep(2000) // todo mock repository instead
+        onView(withId(R.id.manager_list)).check(RecyclerViewItemCountAssertion(0))
+
+        onView(withId(R.id.search_field)).perform(typeText("Ha"))
+        Thread.sleep(2000) // todo mock repository instead
+        onView(withId(R.id.manager_list)).check(RecyclerViewItemCountAssertion(2))
+
+        onView(withId(R.id.search_field)).perform(replaceText("manager"))
+        Thread.sleep(2000) // todo mock repository instead
+        onView(withId(R.id.manager_list)).check(RecyclerViewItemCountAssertion(1))
+
+        onView(withId(R.id.search_field)).perform(replaceText("some random long name"))
+        Thread.sleep(2000) // todo mock repository instead
+        onView(withId(R.id.manager_list)).check(RecyclerViewItemCountAssertion(0))
+
+    }
+}
+
+class RecyclerViewItemCountAssertion(private val expectedCount: Int) : ViewAssertion {
+    override fun check(view: View, noViewFoundException: NoMatchingViewException?) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException
+        }
+        val recyclerView = view as RecyclerView
+        val adapter = recyclerView.adapter
+        assertThat(adapter!!.itemCount, `is`(expectedCount))
+    }
+}
